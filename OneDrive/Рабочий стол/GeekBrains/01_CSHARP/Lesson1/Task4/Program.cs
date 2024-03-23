dae31b1aae52cd8bc14ac479810d43b66f702248
Pavel Golofaev~ <pavel.golofaev@gmail.com~>
2024-03-23 14:18:05 +0300

    lesson1:Task4

---
+++ OneDrive/Рабочий стол/GeekBrains/01_CSHARP/Lesson1/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = 11;
+int b = 16;
+int c = 15;
+int d = 30;
+int e = 10;
+
+int max = a;
+
+if (b > max) 
+{
+    max = b;
+}
+if (c > max) 
+{
+    max = c;
+}
+if (d > max) 
+{
+    max = d;
+}
+if (e > max) 
+{
+    max = e;
+}
+
+Console.WriteLine(max);
+
