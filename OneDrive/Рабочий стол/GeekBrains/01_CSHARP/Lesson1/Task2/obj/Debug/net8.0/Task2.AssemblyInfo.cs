0005fa53842c926f2f3a126e27c62db0cc4f39f5
Pavel Golofaev~ <pavel.golofaev@gmail.com~>
2024-03-23 13:42:08 +0300

    add lesson1:task2

--- OneDrive/Рабочий стол/GeekBrains/01_CSHARP/Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ OneDrive/Рабочий стол/GeekBrains/01_CSHARP/Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c60f8adb3abdaa77614d25812a4cb594c245cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e534c686463fef846176faf91e17e8c38dbfc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
